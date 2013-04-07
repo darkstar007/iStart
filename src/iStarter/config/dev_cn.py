00e133741e94b4c635bf64678481ff039bb5f86d
Chris N <kris_nich@hotmail.co.uk>
2013-04-07 16:01:21 +0100

    CN - removed some vars fromcn settings

--- src/iStarter/config/dev_cn.py
+++ src/iStarter/config/dev_cn.py
@@ -7,13 +7,3 @@ ADMINS = (
      ('Rich Brantingham', 'brantinr@ptn.dstl.uk.vl'),
 )
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dusted',                      # Or path to database file if using sqlite3.
-        'USER': 'dusted',                      # Not used with sqlite3.
-        'PASSWORD': 'dusted',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
