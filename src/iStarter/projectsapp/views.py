d83d59fe71e9f7f7b33627daf47a001d3c6d0f27
Matt Nottingham <matt.nottingham@zen.co.uk>
2013-07-01 10:56:12 +0100

    Really simple start to the projectsapp.

--- src/iStarter/projectsapp/views.py
+++ src/iStarter/projectsapp/views.py
@@ -1,1 +1,31 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+
+import os
+import sys
+import logging
+#============================================================================================
+# TO ENSURE ALL OF THE FILES CAN SEE ONE ANOTHER.
+
+appRoot = os.path.dirname(os.path.realpath(__name__))
+
+# Find out whats in this directory recursively
+for root, subFolders, files in os.walk(appRoot):
+    # Loop the folders listed in this directory
+    for folder in subFolders:
+        directory = os.path.join(root, folder)
+        if directory.find('.git') == -1:
+            if directory not in sys.path:
+                sys.path.append(directory)
+
+#============================================================================================
+
+
+def submit(request):
+    ''' Pulling together ideas into a glorious project. '''
+
+    c = {"classification":"unclassified",
+         "page_title":"My Glorious Project"}
+    c.update(csrf(request))
+    print c
+    return render_to_response('projectsapp/project_submit.html', c)
