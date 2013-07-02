d2d86d0f12075956c736ec2280f37908eb1364b9
Matt Nottingham <matt.nottingham@zen.co.uk>
2013-07-02 07:49:03 +0100

    Latest merge from master.

--- src/iStarter/ideasapp/models.py
+++ src/iStarter/ideasapp/models.py
@@ -23,3 +23,9 @@ class idea(models.Model):
 
     #def __unicode__(self):
         #return self.idea_title
+
+#class ivote(models.Model):
+#    idea = models.ForeignKey(idea)
+#    vote_date = models.DateTimeField('voted_in_date')
+#    username = models.CharField(max_length=128)
+    
