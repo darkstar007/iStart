91b233e309e0e33f777d2cfee3d7441e4a6fe2b0
Rich Brantingham <rich.brantingham@me.com>
2013-07-02 10:43:55 +0100

    added 2 new fields - 1 to handle the existing tags we're putting into the form for users to select from and 1 to handle new tags they may wish to add

--- src/iStarter/ideasapp/forms.py
+++ src/iStarter/ideasapp/forms.py
@@ -6,9 +6,13 @@ class ideaForm(forms.Form):
     
     title  = forms.CharField(widget=forms.TextInput(attrs={'placeholder':"Title?",'class':'span12'}))
     
-    description = forms.CharField(required=True, max_length=200, widget=forms.Textarea(attrs={'placeholder':"What's the idea?",
+    description = forms.CharField(required=True, max_length=500, widget=forms.Textarea(attrs={'placeholder':"What's the idea?",
                                                                                               'class':'span12'}))
     
     cls     = forms.ChoiceField(settings.CLASSIFICATIONS, required=True)
     
-    tags = TagField(widget=forms.TextInput(attrs={'placeholder':"Other tags?",'class':'span12'}))
+    #  These are the tags that a user entered manually
+    new_tags = TagField(widget=forms.TextInput(attrs={'placeholder':"Add your own tags",'class':'span12'}))
+    
+    # These are the tags we presented to them and they clicked on
+    existing_tags = forms.CharField(required=False, widget=forms.HiddenInput)
