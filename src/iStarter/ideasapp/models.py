1a900ff05b07b254b13f6f119caef9e8f4de4819
Chris N <kris_nich@hotmail.co.uk>
2013-07-02 20:16:46 +0100

    fixed the ideas like / dislike functions

--- src/iStarter/ideasapp/models.py
+++ src/iStarter/ideasapp/models.py
@@ -7,11 +7,12 @@ from taggit.managers import TaggableManager
 class idea(models.Model):
     
     title = models.CharField(max_length=200, unique = True)  # The title
-
     pub_date = models.DateTimeField('date_published')
     description = models.CharField(max_length=2000) # The main text
     #idea_id = models.CharField(max_length=100)
     num_backers = models.IntegerField()
+    likes = models.FloatField()
+    dislikes = models.FloatField()
     '''Ommitted these to make life easier at the start...'''
     #names_backers = models.CharField(max_length=20000)
     #name_starter = models.CharField(max_length=100)
@@ -27,7 +28,7 @@ class idea(models.Model):
 class ideaLikes(models.Model):
     title = models.ForeignKey(idea)
     vote_date = models.DateTimeField('voted_in_date')
-    likes = models.IntegerField()
-    dislikes = models.IntegerField()
+    vote_type = models.CharField(max_length=20)
+    
     
    
