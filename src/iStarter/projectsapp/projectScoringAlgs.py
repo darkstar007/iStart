85fa6575186effedca4430877d22206575edb332
Rich Brantingham <rich.brantingham@me.com>
2013-07-04 00:04:32 +0100

    a file for storing custom scoring functions/algs

---
+++ src/iStarter/projectsapp/projectScoringAlgs.py
@@ -0,0 +1,11 @@
+'''
+Store project scoring algorithms in here
+'''
+
+def noddy(importance, resource, effort):
+    ''' Adds the 3 scores together - placeholder '''
+    
+    score = importance * resource * effort
+    return score
+
+#------------------------------------------------------
